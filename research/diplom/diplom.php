93c25d6f29a7584c9afd4b0f7ef2ffd7dfba68df
Brokoli5191 <brokoli5191@gmx.de>
2025-06-24 18:18:18 +0200

    added missing php file

---
+++ research/diplom/diplom.php
@@ -0,0 +1,66 @@
+<?php
+$pageTitle = "Diplom";
+$currentPage = "research";
+include('../../includes/header-courses.php');
+?>
+
+<article>
+  <section class="about-text">
+    <header>
+      <h2 class="h2 article-title">Diplom</h2>
+    </header>
+    <article class="article-second">
+      <h2 class="h3 article-title">Colombeau Theorie und Ultrarelativistischer Limes</h2>
+      <h3>Roland Steinbauer</h3>
+
+Master's Thesis at the <a href="http://www.thp.univie.ac.at/"> 
+Institute for Theoretical Physics</a> of the University of Vienna.<br> 
+Supervised by 
+<a href="http://www.thp.univie.ac.at/local/gravity/people/urbantke.html"> 
+Prof H. Urbantke</a>; November 1995.<br> 109 pages; <b>GERMAN 
+only</b> (sorry!)<br>
+<br>
+<hr>
+<br>
+<h4>Abstract</h4>
+
+This work begins with a short summary of Schwartz's distribution theory. 
+We take a close look at the limitations of this theory, especially at the 
+problems that arise while trying to define a product of distributions. <br> 
+Then follows a presentation of Colombeau's theory of New Generalized  
+Functions, that provides the possibility of a multiplication of  
+distributions. <br>
+In the following chapters we compute the boost and ultrarelativistic limit 
+(v to 1) of the Coulombfield and the Reissner-Nordstrom 
+solution in the space of distributions. Finally we discuss the results 
+involving products of distributions from the viewpoint of Colombeau's theory. 
+<br>
+<br>
+<hr>
+
+<h4>Zusammenfassung</h4>
+
+Die Arbeit beginnt mit einer kurzen Zusammenfassung der Schwartzschen
+Distributionentheorie. Besonders wird dabei auf die Grenzen dieser 
+Theorie eingegangen, vor allem auf die Probleme, die beim Versuch, eine
+Multiplikation von Distributionen zu definieren, auftreten.<br>
+Daran schlieszt sich eine breite Darstellung der Colombeauschen Theorie 
+der verallgemeinerten Funktionen an, in deren Rahmen eine Multiplikation 
+von Distributionen moeglich ist.<br>
+In den folgenden Kapiteln werden Boost und ultrarelativistischer Limes 
+(v gegen 1) von Coulombfeld und 
+Reissner-Nordstrom-Loesung im Raum der Distributionen berechnet. 
+Die dabei auftretenden Fragen im Zusammenhang mit 
+der Multiplikation von Distributionen werden schlieszlich vom Standpunkt 
+der Colombeau-Theorie aus diskutiert und interpretiert. <br>
+<br>
+<hr>
+<br>
+Click  <A HREF="00-dipl.ps">here </a> for the <big>postscript</big> file.
+    </article>
+  </section>
+</article>
+
+<?php
+include('../../includes/footer-courses.php');
+?>
