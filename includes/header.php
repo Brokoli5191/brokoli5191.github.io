5e8e4a5590c6a6926407a4b914622231eec9a8a4
Brokoli5191 <119419930+Brokoli5191@users.noreply.github.com>
2026-02-10 22:58:55 +0100

    v2: fix page layout for refactored pages (close nav + wrap contact in article) and simplify theme toggle markup

--- includes/header.php
+++ includes/header.php
@@ -31,63 +31,9 @@ include($includesPath . 'sidebar.php');
               <button class="<?php echo ($currentPage === 'contact') ? 'navbar-active' : 'navbar-link'; ?>">Contact</button>
             </a>
 
-            <label class="switch">
+            <label class="switch" title="Theme">
               <input id="theme-toggle" type="checkbox" checked />
-              <div class="slider round">
-                <div class="sun-moon">
-                  <svg viewBox="0 0 100 100" class="moon-dot" id="moon-dot-1">
-                    <circle r="50" cy="50" cx="50"></circle>
-                  </svg>
-                  <svg viewBox="0 0 100 100" class="moon-dot" id="moon-dot-2">
-                    <circle r="50" cy="50" cx="50"></circle>
-                  </svg>
-                  <svg viewBox="0 0 100 100" class="moon-dot" id="moon-dot-3">
-                    <circle r="50" cy="50" cx="50"></circle>
-                  </svg>
-                  <svg viewBox="0 0 100 100" class="light-ray" id="light-ray-1">
-                    <circle r="50" cy="50" cx="50"></circle>
-                  </svg>
-                  <svg viewBox="0 0 100 100" class="light-ray" id="light-ray-2">
-                    <circle r="50" cy="50" cx="50"></circle>
-                  </svg>
-                  <svg viewBox="0 0 100 100" class="light-ray" id="light-ray-3">
-                    <circle r="50" cy="50" cx="50"></circle>
-                  </svg>
-
-                  <svg viewBox="0 0 100 100" class="cloud-dark" id="cloud-1">
-                    <circle r="50" cy="50" cx="50"></circle>
-                  </svg>
-                  <svg viewBox="0 0 100 100" class="cloud-dark" id="cloud-2">
-                    <circle r="50" cy="50" cx="50"></circle>
-                  </svg>
-                  <svg viewBox="0 0 100 100" class="cloud-dark" id="cloud-3">
-                    <circle r="50" cy="50" cx="50"></circle>
-                  </svg>
-                  <svg viewBox="0 0 100 100" class="cloud-light" id="cloud-4">
-                    <circle r="50" cy="50" cx="50"></circle>
-                  </svg>
-                  <svg viewBox="0 0 100 100" class="cloud-light" id="cloud-5">
-                    <circle r="50" cy="50" cx="50"></circle>
-                  </svg>
-                  <svg viewBox="0 0 100 100" class="cloud-light" id="cloud-6">
-                    <circle r="50" cy="50" cx="50"></circle>
-                  </svg>
-                </div>
-                <div class="stars">
-                  <svg viewBox="0 0 20 20" class="star" id="star-1">
-                    <path d="M 0 10 C 10 10,10 10 ,0 10 C 10 10 , 10 10 , 10 20 C 10 10 , 10 10 , 20 10 C 10 10 , 10 10 , 10 0 C 10 10,10 10 ,0 10 Z"></path>
-                  </svg>
-                  <svg viewBox="0 0 20 20" class="star" id="star-2">
-                    <path d="M 0 10 C 10 10,10 10 ,0 10 C 10 10 , 10 10 , 10 20 C 10 10 , 10 10 , 20 10 C 10 10 , 10 10 , 10 0 C 10 10,10 10 ,0 10 Z"></path>
-                  </svg>
-                  <svg viewBox="0 0 20 20" class="star" id="star-3">
-                    <path d="M 0 10 C 10 10,10 10 ,0 10 C 10 10 , 10 10 , 10 20 C 10 10 , 10 10 , 20 10 C 10 10 , 10 10 , 10 0 C 10 10,10 10 ,0 10 Z"></path>
-                  </svg>
-                  <svg viewBox="0 0 20 20" class="star" id="star-4">
-                    <path d="M 0 10 C 10 10,10 10 ,0 10 C 10 10 , 10 10 , 10 20 C 10 10 , 10 10 , 20 10 C 10 10 , 10 10 , 10 0 C 10 10,10 10 ,0 10 Z"></path>
-                  </svg>
-                </div>
-              </div>
+              <div class="slider round"></div>
             </label>
           </ul>
 
